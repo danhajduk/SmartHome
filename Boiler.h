5671a42329deb0e696bdb22a77e28d00363fa9d7
danhajduk <Dan.hajduk@gmail.com>
2017-04-28 20:44:16 +0300

    include RF24
    include RF24

--- Boiler.h
+++ Boiler.h
@@ -1,7 +1,7 @@
 /**************************************************************************************
 	Boiler.h - Library for setting up Boiler device
 	Created by Dan Hajduk on April 1, 2017
-	All rights reserved (C)
+	
 
 	Last Modified April 1, 2017
 
@@ -12,6 +12,13 @@
 #ifndef BOILER_H_
 #define BOILER_H_
 
+// include RF24 libs
+#include "RF24.h"
+#include "RF24Network.h"
+#include "RF24Mesh.h"
+#include <SPI.h>
+
+// SmartHome Lib Includes
 #include "Device.h"
 #include "TimerOne.h"
 #include "timeSet.h"
@@ -20,15 +27,15 @@
 
 class Boiler 
 {
+	
 	private:
-		
+
+		RF24 radio(7, 8);
+		RF24Network network(radio);
+		RF24Mesh mesh(radio, network);
 
 		int _pin ;
 		
-		
-		// Setup LCD with assigned pins
-
-
 		void timerIsr();
 		void DrawSCR();
 	public:
--- Device.h
+++ Device.h
@@ -1,7 +1,7 @@
 /**************************************************************************************
 	Device.h - Library for setting up smart home device
 	Created by Dan Hajduk on April 1, 2017
-	
+
 /**************************************************************************************/
 #ifndef Device_h
 #define Device_h
