4dd2be2504c29239fcc5009f2b55b6c8f3e99682
danhajduk <Dan.hajduk@gmail.com>
2017-04-28 21:52:55 +0300

    Fixed NRF24 issues
    Fixed NRF24 compiling issues

--- Boiler.cpp
+++ Boiler.cpp
@@ -11,15 +11,137 @@
 #include "RF24Network.h"
 #include "RF24Mesh.h"
 #include <SPI.h>
+
+#include <LCD5110_Graph.h>
+#include "wifi.h"
+
 #include "Boiler.h"
 
-Boiler::Boiler(int pin)
+RF24 radio(7, 8);									// Init RF24 Radio
+RF24Network network(radio);							// Init RF24 Network
+RF24Mesh mesh(radio, network);						// Init RF24 Mesh
+
+const int _SCK = 7;
+const int _MOSI = 6;
+const int _DC = 5;
+const int _RST = 2;
+const int _CS = 4;
+LCD5110 myGLCD(_SCK, _MOSI , _DC, _RST, _CS );		// Init LCD 
+extern uint8_t SmallFont[];			// Small font pointer
+extern unsigned char TinyFont[];	// Tine font pointer
+
+Boiler::Boiler(int pin ,float Rev , String Last )
 {
 	_pin = pin;
 	dev.setDevice(_pin);					// Set Device Output (on/off)
 	turnOff();
 }
 
+/***************************************************************************************
+	Class Boiler
+	void init () 
+	Turn On Boiler
+/***************************************************************************************/
+void Boiler::init( char nodeID)
+{
+	_nodeID = nodeID;
+	// Connect to the mesh and set Node ID
+	mesh.setNodeID(_nodeID);
+	if (_DEBUG) Serial.println(F("Connecting to the mesh..."));
+	mesh.begin();
+
+	// init LCD and print init data on LCD
+	myGLCD.InitLCD(60);							// Init LCD 55 contrast
+	myGLCD.setFont(SmallFont);					// Set small font
+	myGLCD.clrScr();							// clr screen
+	myGLCD.print("Boiler Device",0,0);			//Print init Data on screen
+	myGLCD.print("Rev :" , 0 , 10);
+	myGLCD.printNumF(_rev,1 ,35,10,'.',1,'0');
+	myGLCD.print(_last,0,40);
+	myGLCD.update();							// Update display
+
+
+	turnOff();
+}
+
+/***************************************************************************************
+	Class Boiler
+	void DrawSCR () 
+	Fills LCD screen with data
+/***************************************************************************************/
+
+void Boiler:: DrawSCR()
+{
+	myGLCD.clrScr();					// clear screen
+	if (Temp < Tmin) Temp = Tmin;
+	TempBar =( (Temp - Tmin)*38) / (Tmax-Tmin) ;
+	
+	// draw frame
+	myGLCD.drawRect(0, 0, 83, 47);
+	myGLCD.drawRect(0, 0, 16, 37);
+	myGLCD.drawRect(0, 0, 16, 47);
+	
+	// draw temperature bar
+	for (int i = 0; i < TempBar ; i++)
+	{
+		myGLCD.drawLine(0, 37 - i,16,37 - i);
+	}
+	
+	// write current Temperature in boiler
+	myGLCD.setFont(SmallFont);
+	myGLCD.printNumI(Temp, 2, 39);
+	
+	// Show current time
+	myGLCD.setFont(SmallFont);
+	myGLCD.print(curentTime.showTime(),20,5);
+	
+	// Show boiler on time (if on)
+	if (boilerTime.isSet)
+	{
+		myGLCD.setFont(TinyFont);
+		myGLCD.print(boilerTime.showTime(),TMRx,TMRy);
+	}
+
+	// update display
+	//backLight(true);
+	if (boilerTime.isSet)
+	{
+		//myGLCD.setFont(SmallFont);
+		//if (_animation) myGLCD.print("((((((",30,25); else myGLCD.print("))))))",30,25);
+		//if (_animation) myGLCD.print("))))))",30,30); else myGLCD.print("((((((",30,30);
+		//myGLCD.drawRect(30, 35, 65, 37);
+		_animation = !_animation;
+		int x1 = 38;
+		int y1 = 15;
+		myGLCD.drawRect(x1,y1,x1+20,y1+22);
+		for (int i = 0; i < 22 ; i++)
+		{
+			myGLCD.drawLine(x1, y1+22- i,x1+20,y1+22 - i);
+		}
+		if (_animation) myGLCD.clrPixel(x1+5,y1+18) ;   else myGLCD.clrPixel(x1+10,y1+18) ;
+		if (_animation) myGLCD.clrCircle(x1+5,y1+18,1); else myGLCD.clrCircle(x1+10,y1+18,1);
+		if (_animation) myGLCD.clrPixel(x1+10,y1+14) ;   else myGLCD.clrPixel(x1+5,y1+14) ;
+		if (_animation) myGLCD.clrCircle(x1+10,y1+14,1); else myGLCD.clrCircle(x1+5,y1+14,1);
+		if (_animation) myGLCD.clrCircle(x1+10,y1+14,2); else myGLCD.clrCircle(x1+5,y1+14,2);
+		if (_animation) myGLCD.clrPixel(x1+5,y1+9) ;   else myGLCD.clrPixel(x1+10,y1+9) ;
+		if (_animation) myGLCD.clrCircle(x1+5,y1+9,1); else myGLCD.clrCircle(x1+10,y1+9,1);
+		if (_animation) myGLCD.clrCircle(x1+5,y1+9,2); else myGLCD.clrCircle(x1+10,y1+9,2);
+		if (_animation) myGLCD.clrCircle(x1+5,y1+9,3); else myGLCD.clrCircle(x1+10,y1+9,3);
+	}
+	else
+	{
+		myGLCD.setFont(TinyFont);
+		myGLCD.print(msg,20,20);
+		myGLCD.print(msg1,20,28);
+	}
+	//backLight(true);
+	//myGLCD.drawBitmap(69,3, Wifi , 13,10);
+	myGLCD.drawLine(70,13,80,3);
+	myGLCD.drawLine(71,13,81,3);
+	//myGLCD.drawCircle(75,7 ,6);
+	myGLCD.update();
+	changed = false;
+}
 /***************************************************************************************
 	Class Boiler
 	void turnOn () 
@@ -45,7 +167,31 @@ void Boiler::turnOff()
 	boilerTime.isSet = false;
 	boilerTime.clearTime();
 }
+/***************************************************************************************
+	Class Boiler
+	void updateBoiler () 
+	Update Boiler status
+/***************************************************************************************/
 
+void Boiler::updateBoiler()
+{
+DrawSCR();
+mesh.update();
+
+}
+
+/***************************************************************************************
+	Class Boiler
+	void backLight (bool On ) 
+	Turns the back light on or off
+/***************************************************************************************/
+
+void Boiler::backLight (bool On )
+{
+	if (On)   analogWrite(_backLightPin,ON);
+	else   analogWrite(_backLightPin,OFF);
+	lightOn = On;
+}
 
 
 
--- Boiler.h
+++ Boiler.h
@@ -29,25 +29,44 @@ class Boiler
 {
 	
 	private:
+		const float _rev = 0.1;
+		const String _last = "09/04/2017";
 
-		RF24 radio(7, 8);
-		RF24Network network(radio);
-		RF24Mesh mesh(radio, network);
+		const int Tmax = 60, Tmin = 20;		// Temperature limits
+		int TempBar, Time1 ;				//
+		const int TMRx = 33, TMRy = 41;		//X,Y coordinates of the Timer on
+		const int _backLightPin = 3;		// Back light on pin 3
+		const int _lightON = 180;			// Light on level
+		const int _lightOff = 255;			// light off level
+		int Temp = 00;						// boiler temperature
+		const int ON = 180, OFF= 255, Low = 220;
 
-		int _pin ;
+		bool _animation = false;  
 		
+
+		bool _DEBUG;
+		int _pin ;
+		char _nodeID ;
+
 		void timerIsr();
-		void DrawSCR();
+		
 	public:
 		Device dev;
 		bool changed ;
 		tSet curentTime ;
 		tSet boilerTime ;
 		int temperature = 40;
+		bool oneSecPassed = false;
+		String msg,msg1;
+		bool lightOn;
 
-		Boiler (int pin );
+		Boiler (int pin ,float Rev , String Last );
+		void init (char nodeID);
 		void turnOn();
 		void turnOff();
+		void DrawSCR();
+		void updateBoiler();
+		void backLight (bool On );
 };
 
 
